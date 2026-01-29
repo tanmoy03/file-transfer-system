e39f8e6c21790a02b507f4b31387f4d965364485
Tanmoy <121324683@umail.ucc.ie>
2026-01-29 22:51:29 +0000

    basic file transfer working using sockets

--- client/client_main.py
+++ client/client_main.py
@@ -1,1 +1,25 @@
+import socket
+import os
+
+SERVER_IP = "127.0.0.1"
+PORT = 5001
+BUFFER_SIZE = 4096
+
+file_path = input("Enter file path: ")
+filename = os.path.basename(file_path)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER_IP, PORT))
+
+client.send(filename.encode())
+
+with open(file_path, "rb") as f:
+    while True:
+        data = f.read(BUFFER_SIZE)
+        if not data:
+            break
+        client.sendall(data)
+
+print("File sent successfully.")
+client.close()
 
--- server/server_main.py
+++ server/server_main.py
@@ -1,1 +1,31 @@
+import socket
+import os
+
+HOST = "0.0.0.0"
+PORT = 5001
+BUFFER_SIZE = 4096
+
+os.makedirs("received_files", exist_ok=True)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen(1)
+
+print("Waiting for connection...")
+
+conn, addr = server.accept()
+print("Connected:", addr)
+
+filename = conn.recv(BUFFER_SIZE).decode()
+
+with open("received_files/" + filename, "wb") as f:
+    while True:
+        data = conn.recv(BUFFER_SIZE)
+        if not data:
+            break
+        f.write(data)
+
+print("File received successfully.")
+conn.close()
+server.close()
 
