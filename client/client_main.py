05f7be412fdcb65432c1043fe9f7756eba9b496b
Tanmoy <121324683@umail.ucc.ie>
2026-02-06 13:41:42 +0000

    added initial UDP connection code

--- client/client_main.py
+++ client/client_main.py
@@ -1,25 +1,49 @@
-import socket
-import os
+# import socket
+# import os
 
-SERVER_IP = "127.0.0.1"
-PORT = 5001
-BUFFER_SIZE = 4096
+# SERVER_IP = "127.0.0.1"
+# PORT = 5001
+# BUFFER_SIZE = 4096
 
-file_path = input("Enter file path: ")
-filename = os.path.basename(file_path)
+# file_path = input("Enter file path: ")
+# filename = os.path.basename(file_path)
+ 
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client.connect((SERVER_IP, PORT))
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((SERVER_IP, PORT))
+# client.send(filename.encode())
 
-client.send(filename.encode())
+# with open(file_path, "rb") as f:
+#     while True:
+#         data = f.read(BUFFER_SIZE)
+#         if not data:
+#             break
+#         client.sendall(data)
 
-with open(file_path, "rb") as f:
-    while True:
-        data = f.read(BUFFER_SIZE)
-        if not data:
-            break
-        client.sendall(data)
+# # add try catch block error handling
+# # seperate the cli functionality from the file transfer functionality
+# # add a popup with a file explorer to choose file to send
+# # implement a login system with a server side user database
 
-print("File sent successfully.")
-client.close()
 
+# print("File sent successfully.")
+# client.close()
+
+from connection import create_udp_socket
+
+SERVER_IP = input("Enter server IP: ")
+SERVER_PORT = 5001
+
+sock = create_udp_socket()
+
+while True:
+    msg = input("Enter message (or quit): ")
+
+    if msg == "quit":
+        break
+
+    sock.sendto(msg.encode(), (SERVER_IP, SERVER_PORT))
+
+    data, addr = sock.recvfrom(1024)
+
+    print("Server replied:", data.decode())
--- server/server_main.py
+++ server/server_main.py
@@ -1,31 +1,52 @@
-import socket
-import os
+# import socket
+# import os
 
-HOST = "0.0.0.0"
-PORT = 5001
-BUFFER_SIZE = 4096
+# HOST = "0.0.0.0"
+# PORT = 5001
+# BUFFER_SIZE = 4096
 
-os.makedirs("received_files", exist_ok=True)
+# os.makedirs("received_files", exist_ok=True)
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen(1)
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.bind((HOST, PORT))
+# server.listen(1)
 
-print("Waiting for connection...")
+# print("Waiting for connection...")
 
-conn, addr = server.accept()
-print("Connected:", addr)
+# conn, addr = server.accept()
+# print("Connected:", addr)
 
-filename = conn.recv(BUFFER_SIZE).decode()
+# filename = conn.recv(BUFFER_SIZE).decode()
 
-with open("received_files/" + filename, "wb") as f:
-    while True:
-        data = conn.recv(BUFFER_SIZE)
-        if not data:
-            break
-        f.write(data)
+# with open("received_files/" + filename, "wb") as f:
+#     while True:
+#         data = conn.recv(BUFFER_SIZE)
+#         if not data:
+#             break
+#         f.write(data)
 
-print("File received successfully.")
-conn.close()
-server.close()
+# print("File received successfully.")
+# conn.close()
+# server.close()
 
+from connection import create_udp_socket
+
+SERVER_IP = "0.0.0.0"
+SERVER_PORT = 5001
+
+sock = create_udp_socket()
+sock.bind((SERVER_IP, SERVER_PORT))
+
+print("UDP Server running...")
+print(f"Listening on port {SERVER_PORT}")
+
+while True:
+    data, addr = sock.recvfrom(1024)
+
+    message = data.decode()
+
+    print(f"Received from {addr}: {message}")
+
+    # Reply to client
+    response = f"ACK: {message}"
+    sock.sendto(response.encode(), addr)
